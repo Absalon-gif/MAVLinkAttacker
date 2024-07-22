8d9fabd8ae7e714934164e8512a1f5205873ec83
absalon-gif <abbsalon2000@gmail.com>
2024-07-22 23:41:52 +0200

    Code for the attacker

---
+++ src/packet_storage.py
@@ -0,0 +1,16 @@
+import struct
+
+
+class PacketStorage:
+    def __init__(self):
+        self.mav_packets = list
+
+
+    def store_packet(self, mav_packet, addr):
+        packet_info = {
+
+        }
+        return
+
+    def retrieve_packet(self):
+        return
